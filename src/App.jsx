1588a40e00975d0dadc225977b172414e84924f5
Sasha Pavlenko <spavlenko.voc@gmail.com>
2025-10-10 18:03:28 +0200

    add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-function App () {
-    return <h1>hello bro</h1>;
-}
+function App() {
+    return (
+    <div>
+        <h1>hello bro</h1>
+        <button>Click me!</button>
+    </div>
+    );
+};
 
 export default App;
