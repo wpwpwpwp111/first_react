264b07fb053948f3a68361fe19d0b2c1bba3d165
Sasha Pavlenko <spavlenko.voc@gmail.com>
2025-10-10 23:00:50 +0200

    add const

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 function App() {
+    const message="whaaaaat"
     return (
     <div>
-        <h1>hello bro</h1>
+        <h1>hello bro {message}</h1>
         <button>Click me!</button>
         <p>141551617687871661218287126212689732782739237823</p>
+        <input type="texty"/>
     </div>
     );
 };
